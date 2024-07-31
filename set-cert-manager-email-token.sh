b094da9bb9c56c23960eb214d57e1418ba57bf68
jakubmkowalski <jakub.kowalski@4chain.studio>
2024-07-31 09:34:14 +0200

    feat(SPV-894): switches name of api key to default name

--- set-cert-manager-email-token.sh
+++ set-cert-manager-email-token.sh
@@ -4,5 +4,5 @@ read -sp "Enter your cloudflare dns edit access token: " CLOUDFLARE_TOKEN
 
 sudo microk8s kubectl create secret generic cloudflare-api-token-secret \
   --namespace default \
-  --from-literal=cloudflare-api-token="${CLOUDFLARE_TOKEN}"
+  --from-literal=api-token="${CLOUDFLARE_TOKEN}"
 
